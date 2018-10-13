3dcc26566f52c1bbe6f8cabf29bbd8badf0a0130
max-golubev <max.k.golubev@gmail.com>
2018-10-13 17:23:16 +0200

    Split game - v0.0.2
    - more checks for small cells

--- src/BoardTest.py
+++ src/BoardTest.py
@@ -1,6 +1,8 @@
 import unittest
-import Board
+
 import BigCell
+import Board
+from BigCell import BigCell
 
 class BoardTest(unittest.TestCase):
 
@@ -8,15 +10,16 @@ class BoardTest(unittest.TestCase):
         board = Board.Board(4, 3)
         top_left = self.check_cell(board, 0, 0)
         self.assertEqual(True, top_left.has_bottom())
+        self.assertEqual(True, top_left.has_right())
+        self.assertEqual(False, top_left.has_left())
         self.assertEqual(False, top_left.has_top())
 
     def test_another_cell(self):
         board = Board.Board(24, 53)
         self.check_cell(board, 14, 32)
 
-
-    def check_cell(self, board, x, y):
+    def check_cell(self, board, x, y) -> BigCell:
         result = board.get_cell(x, y)
         self.assertEqual(x, result.get_x())
         self.assertEqual(y, result.get_y())
-        return  result
+        return result
