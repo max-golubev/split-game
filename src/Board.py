3c5df821bdcbe9540dc7f4c754c5d87525e09c78
max-golubev <max.k.golubev@gmail.com>
2019-04-07 18:07:50 +0200

    split-game.py
    -add `compute_next_player`

--- src/Board.py
+++ src/Board.py
@@ -16,6 +16,22 @@ class Board:
     def get_current_player(self):
         return self.current_player
 
+    def start_next_turn(self):
+        self.current_player = self.compute_next_player()
+        return self.get_current_player()
+
+    def compute_next_player(self):
+        current = self.get_current_player()
+        if current == PLAYER_1:
+            return PLAYER_2
+        if current == PLAYER_2:
+            return PLAYER_3
+        if current == PLAYER_3:
+            return PLAYER_4
+        if current == PLAYER_4:
+            return PLAYER_1
+        raise "Unknown player: " + repr(current)
+
     def get_cell(self, col, row) -> BigCell:
         return self.cells[col][row]
 
